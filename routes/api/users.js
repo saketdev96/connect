8f011a874745ef654b7920a0c0127ef87611f5bd
Quock <saketp96@utexas.edu>
2018-10-16 19:25:35 -0500

    added password hashing

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 
 
 //Load User model
@@ -27,13 +28,23 @@ router.post('/register', (req,res) =>{
                 r: 'x',
                 d:'mn'
             });
-            
+
             const newUser = new User({
                 name: req.body.name,
                 email: req.body.email,
                 avatar,
                 password: req.body.password
             });
+
+            bcrypt.genSalt(10,(err, salt) =>{
+                bcrypt.hash(newUser.password,salt, (err, hash) => {
+                    if (err) throw err;
+                    newUser.password = hash;
+                    newUser.save()
+                    .then(user => res.json(user))
+                    .catch(err => console.log(err));
+                })
+            })
         }
     })
 });
